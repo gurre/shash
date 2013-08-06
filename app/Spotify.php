bae9f516f838da053236c188e7f1c730a0fcabd6
Gustav Svalander <gsvalander@gmail.com>
2013-08-06 17:40:35 +0200

    dev

--- app/Spotify.php
+++ app/Spotify.php
@@ -1,8 +1,13 @@
 <?php
-
+$memcache_obj = new Memcache;
+$memcache_obj->connect('localhost', 11211);
 class Spotify {
 	
 	static function track($q){
+		global $memcache_obj;
+		if( ($re = $memcache_obj->get( array($q) ) !== false ){
+			return $re;
+		}
 		
 		$resp=file_get_contents("http://ws.spotify.com/search/1/track.json?q=$q");
 		if($resp==null){
@@ -12,6 +17,7 @@ class Spotify {
 		if( !isset($obj->tracks) || empty($obj->tracks) ){
 			return array();
 		}
+		$memcache_obj->set($q, $obj->tracks, 0, 3600);
 		return $obj->tracks;
 	}
 	
