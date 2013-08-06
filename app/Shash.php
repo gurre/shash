a964a8c16c99a14fd37567bc8c8982088663f7c7
Gustav Svalander <gsvalander@gmail.com>
2013-08-06 12:59:30 +0200

    added echonest api

---
+++ app/Shash.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Your API Key: BBMRBVEOASYLHY60X 
+Your Consumer Key: 331db9732552f9b3a55bb6b41fd63552 
+Your Shared Secret: KbC5Y0s3SK2C87YtNGA4Bg
+*/
+require 'Redis.php';
+
+
+class Shash {
+
+	static function spotifyIdForTag($spotify_id){
+		return Redis::db->hget($spotify_id);
+	}
+	
+	static function tagsForText($text){
+		require '../app/php-echonest-api/lib/EchoNest/Client.php';
+		
+		$echonest = new EchoNest_Client();
+		$echonest->authenticate('BBMRBVEOASYLHY60X');
+		$songApi = $echonest->getSongApi();
+		$songs=$songApi->search( array('title'=>$text) );
+		print_r($songs);
+		if( !empty($songs) ){
+			return array();
+		}
+		
+		foreach( $songs as $song ){
+			
+			
+		}
+		
+		
+	}
+
+}
--- app/query.php
+++ app/query.php
@@ -1,2 +1,26 @@
 <?php
 
+require 'Shash.php';
+
+/*
+spotify_id -> tags
+
+spotify:track:12 : {
+	123123 : (int)123123
+}
+
+ids : {
+	(int)123123 : tag
+}
+
+tags : {
+	tag : (int)123123
+}
+*/
+
+
+if( isset($_GET['text']) && ($query=addslashes($_GET['text']))!==false ){
+	
+	Redis::db()->hExists();
+	
+}
