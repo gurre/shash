66224b77c6ff52446b976c60b3148a851120c3ee
Gustav Svalander <gsvalander@gmail.com>
2013-08-06 16:52:07 +0200

    dev

--- app/Shash.php
+++ app/Shash.php
@@ -103,7 +103,7 @@ function slug($str, $replace=array(), $delimiter='') {
 	if( !empty($replace) ) {
 		$str = str_replace((array)$replace, ' ', $str);
 	}
-	$clean = ucwords($clean);
+	$str = ucwords($str);
 	$clean = iconv('UTF-8', 'ASCII//TRANSLIT', $str);
 	$clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
 	$clean = strtolower(trim($clean, '-'));
