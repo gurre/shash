4a841637a6e694ffe5af80219270a657a49ca047
Gustav Svalander <gsvalander@gmail.com>
2013-08-06 17:29:35 +0200

    dev

--- app/query.php
+++ app/query.php
@@ -7,7 +7,7 @@
 if( isset($_GET['text']) && ($text=addslashes($_GET['text']))!==false ){
 	
 	$obj=Shash::tagsForText($text);
-	if( ($callback=Sanitize::match_implode(REGEX_ALPHANUMERICSIGNS,isIdxSetNotNull($_GET,"callback"))) ){
+	if( ($callback=$_GET["callback"]) !==false) ){
 		header('Content-Type: application/javascript');
 		echo $callback.'('.json_encode($obj).')';
 	}else{
